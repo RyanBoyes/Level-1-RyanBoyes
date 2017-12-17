5ff510d5f81c57f47ab19c8241773b0e9a4a1615
TJHL <league@WTS-IM-6.attlocal.net>
2017-12-17 11:20:12 -0800

    yeet pt. 2
    More on GetLatestTweet

--- src/GetLatestTweet.java
+++ src/GetLatestTweet.java
@@ -1,4 +1,47 @@
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class GetLatestTweet {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class GetLatestTweet implements ActionListener {
+	JFrame frame;
+	JPanel panel;
+	JButton button;
+	JTextField textfield;
+	
+	public void CreateUI() {
+		frame = new JFrame();
+		panel = new JPanel();
+		button = new JButton("Search The Twitterverse");
+		textfield = new JTextField();
+		frame.add(panel);
+		panel.add(button);
+		panel.add(textfield);
+		frame.pack();
+		frame.setVisible(true);
+		button.addActionListener(this);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+
+
+
+
+
+
+
+
+
+
 
 }
