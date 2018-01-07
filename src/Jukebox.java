795394957a28cc407d7e576ba0e4275051d8657f
RyanBoyes <league@WTS-IM-6.attlocal.net>
2018-01-07 11:27:48 -0800

    Jukebox, Candyman IP
    Sick beats on dat jukebox, candyman is a dumb movie

--- src/Jukebox.java
+++ src/Jukebox.java
@@ -1,5 +1,6 @@
 // Copyright The League of Amazing Programmers, 2015
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -20,18 +21,20 @@ import javazoom.jl.player.advanced.AdvancedPlayer;
 /* 1. Download the JavaZoom jar from here: http://bit.ly/javazoom
  * 2. Right click your project and add it as an External JAR (Under Java Build Path > Libraries).*/
 
-public class Jukebox implements Runnable {
+public class Jukebox implements Runnable, ActionListener {
 	public void run() {
 
 		// 3. Find an mp3 on your computer or on the Internet.
 		// 4. Create a Song
-        	  Song Song1 = new Song("Song.mp3");
-        	  Song Song2 = new Song("Song2.mp3");
-        	   
+        	   Song1 = new Song("Song.mp3");
+        	   Song2 = new Song("Song2.mp3");
+        	   Song3 = new Song("Song3.mp3");
+        	   Song4 = new Song("Song4.mp3");
+        	   Song5 = new Song("Song5.mp3");
         	   
 
 		// 5. Play the Song
-        	  Song1.play();
+        	  
 
 		/*
 		 * 6. Create a user interface for your Jukebox so that the user can to
@@ -40,28 +43,47 @@ public class Jukebox implements Runnable {
 		 * cover is clicked, stop the currently playing song, and play the one
 		 * that was selected.
 		 */
-          }
+	
+        	  frame = new JFrame();
+        		panel = new JPanel();
+        		button1 = new JButton("Meme Song");
+        		button2 = new JButton("Unicorn Song");
+        		button3 = new JButton("Cat Song");
+        		button4 = new JButton("Cool Song");
+        		button5 = new JButton("Another Cool Song");
+        		button1.addActionListener(this);
+        		button2.addActionListener(this);
+        		button3.addActionListener(this);
+        		button4.addActionListener(this);
+        		button5.addActionListener(this);
+        		frame.setVisible(true);
+        		frame.add(panel);
+        		panel.add(button1);
+        		panel.add(button2);
+        		panel.add(button3);
+        		panel.add(button4);
+        		panel.add(button5);
+        		frame.pack();
+
+	}
 	JFrame frame;
 	JPanel panel;
 	JButton button1;
 	JButton button2;
+	JButton button3;
+	JButton button4;
+	JButton button5;
 	Song Song1;
 	Song Song2;
-	public void CreateUI() {
-		
-		frame = new JFrame();
-		panel = new JPanel();
-		button1 = new JButton("Meme Song");
-		button2 = new JButton("Unicorn Song");
-		frame.setVisible(true);
-		frame.pack();
-		frame.add(panel);
-		panel.add(button1);
-		panel.add(button2);
-	}
+	Song Song3;
+	Song Song4;
+	Song Song5;
+	
+	
+
 
 	public static void main(String[] args) {
-		SwingUtilities.invokeLater(new Jukebox().CreateUI());
+		SwingUtilities.invokeLater(new Jukebox());
 	}
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
@@ -70,6 +92,15 @@ public class Jukebox implements Runnable {
 		}
 		else if(e.getSource()==button2) {
 			Song2.play();
+		}
+		else if(e.getSource()==button3) {
+			Song3.play();
+		}
+		else if(e.getSource()==button4) {
+			Song4.play();
+		}
+		else if(e.getSource()==button5) {
+			Song5.play();
 		}
 	}
 
