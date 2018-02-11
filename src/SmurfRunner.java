b31a6d36768e045c2191c3c5fc4c2c031f89b3f2
RyanBoyes <league@WTS-IM-6.attlocal.net>
2018-02-11 11:19:38 -0800

    Smurfs and Backpacks
    You know it.

---
+++ src/SmurfRunner.java
@@ -0,0 +1,45 @@
+
+public class SmurfRunner {
+
+	public static void main(String[] args) {
+		Smurf handy = new Smurf("Handy");
+		System.out.println(handy.getName());
+		handy.eat();
+
+		Smurf papa = new Smurf("Papa Smurf");
+		System.out.println(papa.getName());
+		
+		
+		Smurf ette = new Smurf("Smurfette");	
+		
+		System.out.println(papa.getHatColor());
+		System.out.println(ette.getHatColor());
+		System.out.println(papa.isGirlOrBoy());
+		System.out.println(ette.isGirlOrBoy());
+		}
+			
+		
+		
+	
+	
+	
+	
+	
+	
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
